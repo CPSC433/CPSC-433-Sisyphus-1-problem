0e4ac9c405040af76aae1943e6286ec1280829cf
Kristi <kristi.mikkelsen@shaw.ca>
2013-03-26 18:31:16 -0600

    Functions added in Room class

--- Room.java
+++ Room.java
@@ -5,14 +5,33 @@ import java.util.ArrayList;
 public class Room 
 {
 	private String name;
-	private int num_occupants;
 	private ArrayList<Person> occupants;
 	private ArrayList<Room> close_to;
+	
+	
 	
 	public Room(String roomName) 
 	{
 		name = roomName;
+		occupants.add(null);
+		occupants.add(null);
 	}
 	
+	public boolean isShared() {
+		if ((occupants.get(0) == null) || (occupants.get(1) == null))
+			return false;
+		else
+			return true;
+	}
 	
+	public Person getOther(Person person1) {
+		if (num_occupants == 2) {
+			if (occupants.get(0) != person1)
+				return occupants.get(0);
+			else
+				return occupants.get(1);
+		}
+		else
+			
+	}
 }
