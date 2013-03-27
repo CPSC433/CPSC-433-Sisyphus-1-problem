da3b6da33607a2341bce36d6d5b37c74cd24e6f7
CPSC433 <mclenahankyle@hotmail.com>
2013-03-26 18:59:59 -0600

    Updated person class

--- Person.java
+++ Person.java
@@ -4,19 +4,97 @@ import java.util.ArrayList;
 
 public class Person 
 {
-  private String name;
+	private String name;
 	private String group;
 	private String project;
 	private boolean isProjectHead;
+	private boolean isGroupHead;
 	private boolean isHacker;
 	private boolean isSmoker;
-	
 	private ArrayList<String> arrWorksWith = new ArrayList<String>();
 	
 	public Person(String personName) 
 	{
 		name = personName;
+		group = "";
+		project = "";
+		isProjectHead = false;
+		isGroupHead = false;
+		isHacker = false;
+		isSmoker = false;
 	}
 	
+	// Getters
 	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getGroup()
+	{
+		return group;
+	}
+	
+	public String getProject()
+	{
+		return project;
+	}
+	
+	public boolean getIsGroupHead()
+	{
+		return isGroupHead;
+	}
+	
+	public boolean getIsProjectHead()
+	{
+		return isProjectHead;
+	}
+	
+	public boolean getIsHacker()
+	{
+		return isHacker;
+	}
+	
+	public boolean getIsSmoker()
+	{
+		return isSmoker;
+	}
+	
+	// Setters
+	
+	public void setGroup( String name )
+	{
+		group = name;
+	}
+	
+	public void setProject( String name )
+	{
+		project = name;
+	}
+	
+	public void setIsGroupHead( boolean b )
+	{
+		isGroupHead = b;
+	}
+	
+	public void setIsProjectHead( boolean b )
+	{
+		isProjectHead = b;
+	}
+	
+	public void setIsHacker( boolean b )
+	{
+		isHacker = b;
+	}
+	
+	public void setIsSmoker( boolean b )
+	{
+		isSmoker = b;
+	}
+	
+	public void addWorksWith( String name )
+	{
+		arrWorksWith.add( name );
+	}
 }
